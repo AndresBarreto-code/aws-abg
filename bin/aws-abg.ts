d484e2f31466b0a9f22142f89db9a41132997aa3
Andres Barreto <andres.barreto@koombea.com>
2021-06-05 21:39:24 -0500

    first steps

--- bin/aws-abg.ts
+++ bin/aws-abg.ts
@@ -2,20 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AwsAbgStack } from '../lib/aws-abg-stack';
+import { ProcessEnv } from '../interfaces/ProcessEnv.interface';
+
+const { STACK_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY, CDK_DEFAULT_REGION } = process.env as ProcessEnv;
 
 const app = new cdk.App();
-new AwsAbgStack(app, 'AwsAbgStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new AwsAbgStack(app, STACK_NAME, {
+  env: { account: ACCESS_KEY_ID, region: CDK_DEFAULT_REGION },
 });
--- lib/aws-abg-stack.ts
+++ lib/aws-abg-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
 
 export class AwsAbgStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
